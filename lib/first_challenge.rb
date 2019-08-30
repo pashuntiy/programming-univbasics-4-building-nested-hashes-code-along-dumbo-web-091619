f8d5469da30dd76d96198866930bf59cc57276d4
Pavel Ilin <pavel_ilin@yahoo.com>
2019-08-30 19:18:37 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     :montague +> {
+     :montague => {
        :patriarch => {name: "Lord Montague", age: "53"},
        :matriarch => {name: "Lady Montague", age: "54"},
        :hero => {name: "Romeo", age: "15", status: "alive"},
@@ -8,7 +8,7 @@ def first_challenge
          {name: "Bonvolio", age: "17", attitude: "worried"},
          {name: "Mercutio", age: "18", attitude: "hot-headed"}]
      },
-     :capulet +> {
+     :capulet => {
        :patriarch => {name: "Lord Capulet", age: "50"},
        :matriarch => {name: "Lady Capulet", age: "51"},
        :heroine => {name: "Juliet", age: "15", status: "alive"},
